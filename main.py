765beda9e3c11cc18d41729c3099ee12d2d813ba
diyorbekjumanov <diyorbekjumanov1120@gmail.com>
2021-02-17 17:00:48 +0000

    collection

--- main.py
+++ main.py
@@ -4,11 +4,14 @@ from pprint import pprint
 def document(dt_txt):
     doc_list = dt_txt.split('\n')
     doc_keys = doc_list[0].split(',')[1:]
-
+    collection = list()
     for row in doc_list[1:]:
         doc_full = dict()
         for i, x in enumerate(row.split(',')[1:]):
-            print(i, x)
+            doc_full[doc_keys[i]] = x
+        collection.append(doc_full)
+    
+    pprint(collection)
         
 
 
