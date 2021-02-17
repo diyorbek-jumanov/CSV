88aac9840580e5341fa5cc2f989de44143ccf2af
diyorbekjumanov <diyorbekjumanov1120@gmail.com>
2021-02-17 16:35:25 +0000

    read

--- main.py
+++ main.py
@@ -1,19 +1,10 @@
-from tinydb import TinyDB
+from tinydb import TinyDB, Query
+from pprint import pprint
 
-def insert_db(data_csv):
+data1 = open('products.csv')
+data2 = open('specifications.csv')
 
-    products_db = []
-    for row in data_csv.split('\n')[1:]:
-        cat,company,price = row.split(',')[1:]
-        products_db.append({'model':cat,'company':company, 'price': int(price)})
-        print(row)
-
-    return products_db
-
-db = TinyDB('db.json')
-data = open('specifications.csv').read()
-
-db.truncate()
-products_db = insert_db(data)
-
-db.insert_multiple(products_db)
+data1_text = data1.read()
+pprint(data1_text)
+data2_text = data2.read()
+pprint(data2_text)
